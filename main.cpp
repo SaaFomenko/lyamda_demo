3aa03da44dac259768e326572adcb31708840901
SaaFomenko <s.fomenko@list.ru>
2023-03-23 14:03:08 +0300

    Save lyamda_demo first work version.

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	const std::string in_data = "Входные данные: ";
+	const std::string out_data = "Выходные данные: ";
+	std::vector<int> arr_vect = { 4, 7, 9, 14, 12 };
+
+	std::cout << in_data;
+	std::for_each(
+			arr_vect.begin(),
+			arr_vect.end(),
+			[](int& i)
+			{
+				std::cout << i << ' ';
+			}
+			);
+	std::cout << std::endl;
+
+	std::cout << out_data;
+	std::for_each(
+			arr_vect.begin(),
+			arr_vect.end(),
+			[](int& i)
+			{
+				bool check = i % 2 == 0 ? false : true;
+
+				if (check) i = i * 3;
+
+				std::cout << i << ' ';
+			}
+			);
+	std::cout << std::endl;
+
+
+
+	return 0;
+}
